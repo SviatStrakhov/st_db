6e3bac9664816a7d8562a3ef3ebab2078952e0a0
Svo Hurts <svoharts@gmail.com>
2017-05-15 20:37:59 +0300

    urls and tags

--- students/views.py
+++ students/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views Students
+def students_list(request):
+	return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+	return HttpResponse('ADD')
+
+def students_edit(request, sid):
+	return HttpResponse('edit huy')
+
+def students_delete(request, sid):
+	return HttpResponse('delete' % sid)
+
+
+
+#Views Groups
+def groups_list(request):
+	return HttpResponse('<h1>Groups Listing</h1>')
+
+def groups_add(request):
+	return HttpResponse('<h1>add</h1>')
+
+def groups_edit(request, gid):
+	return HttpResponse('<h1>Edit</h1>' % gid)
+
+def groups_delete(request, gid):
+	return HttpResponse('<h1>delete</h1>' % gid)
+
+
+
+def journal(request):
+	return HttpResponse('<h1>Journal</h1>')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-
+from students.views import *
 urlpatterns = [
+	# Students urls
+	url(r'^$', students_list, name='home'),
+	url(r'^students/add/$', students_add, name='students_add'),
+	url(r'^students/(?P<sid>\d+)/edit/$', students_edit, name='students_edit'),
+	url(r'^students/(?P<sid>\d+)/delete/$', students_delete, name='students_delete'),
+
+	#Groups urls
+	url(r'^groups/$', groups_list, name='groups'),
+	url(r'^groups/add/$', groups_add, name='groups_add'),
+	url(r'^groups/(?P<gid>\d+)/edit/$', groups_edit, name='groups_edit'),
+	url(r'^groups/(?P<sid>\d+)/delete/$', groups_delete, name='groups_delete'),
+
+	
+	url(r'^journal/$', journal, name='journal'),
+	#url(r'/journsl/125'),
+	#url(r'/journal/update'),
+
     url(r'^admin/', admin.site.urls),
 ]
